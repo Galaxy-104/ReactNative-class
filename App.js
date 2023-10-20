064b11c667a7f1787ca991cca0e547fdae9f256e
Galaxy <js_park95@naver.com>
2023-10-20 11:35:06 +0900

    Feat: 하단탭 생성
     각 페이지로 이동하는 하단탭 생성

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import HomeScreen from './screens/HomeScreen'
 import CalandarScreen from './screens/CalandarScreen'
 import DashBoardScreen from './screens/DashBoardScreen'
 import SettingsScreen from './screens/SettingsScreen'
 
-const Stack = createNativeStackNavigator()
+// const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
 
 function App(){
 
   return (
     <NavigationContainer>
       {/* 내비게이션 설정 */}
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='Calendar' component={CalandarScreen}/>
-        <Stack.Screen name='DashBoard' component={DashBoardScreen}/>
-        <Stack.Screen name='Settings' component={SettingsScreen}/>
-      </Stack.Navigator>  
+      <Tab.Navigator initialRouteName='Home'>
+        <Tab.Screen name='Home' component={HomeScreen} options={{
+          title: '홈',
+          tabBarIcon: ({ color, size }) => <Icon name='home' color={color} size={size}/>
+        }}/>
+        <Tab.Screen name='Calendar' component={CalandarScreen} options={{
+          title: '달력',
+          tabBarIcon: ({ color, size }) => <Icon name='calandar-today' color={color} size={size}/>          
+        }}/>
+        <Tab.Screen name='DashBoard' component={DashBoardScreen} options={{
+          title: '통계',
+          tabBarIcon: ({ color, size }) => <Icon name='dashboard' color={color} size={size}/>          
+        }}/>
+        <Tab.Screen name='Settings' component={SettingsScreen} options={{
+          title: '설정',
+          tabBarIcon: ({ color, size }) => <Icon name='settings' color={color} size={size}/>          
+        }}/>
+      </Tab.Navigator>  
     </NavigationContainer>
   )
 }
