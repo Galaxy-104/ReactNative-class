819d7f9c533b485cffb8e5f0c0ea393019fef34c
Galaxy <js_park95@naver.com>
2023-10-20 12:37:30 +0900

    Feat: 오늘 날짜 보여주기
     오늘 날짜 보여주기 위한  DateHeader 컴포넌트 생성
     홈 화면에 추가

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { SafeAreaView, View, Text, StyleSheet, StatusBar } from 'react-native'
 
+import DateHeader from '../components/DateHeader'
+
 function HomeScreen({navigation}){
+
+    const date = new Date()
+    
     return (
         <SafeAreaView style={styles.block}>
             <StatusBar backgroundColor="#a8c8ffff"></StatusBar>
+            <DateHeader date={date}/>
             <View>
                 <Text>할일 목록</Text>
             </View>
