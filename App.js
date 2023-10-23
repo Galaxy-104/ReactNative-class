1a6f07f934b2fa88c0f6ae4d1dbd7adaae110f7f
Galaxy <js_park95@naver.com>
2023-10-23 11:00:32 +0900

    feat: 카테고리 선택창
     드롭다운 카테고리 메뉴 추가
     카테고리 미선택 에러 메시지

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -8,18 +10,30 @@ import CalendarScreen from './screens/CalendarScreen'
 import DashBoardScreen from './screens/DashBoardScreen'
 import SettingsScreen from './screens/SettingsScreen'
 
+import DropdownCategory from './components/DropdownCategory'
+
 // const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
 function App(){
 
+  const [caretType, setCaretType] = useState(false)
+
   return (
     <NavigationContainer>
       {/* 내비게이션 설정 */}
       <Tab.Navigator initialRouteName='Home'>
-        <Tab.Screen name='Home' component={HomeScreen} options={{
+        <Tab.Screen name='Home' children={(props) => <HomeScreen {...props} caretType={caretType} setCaretType={setCaretType}/> } options={{
           title: '홈',
-          tabBarIcon: ({ color, size }) => <Icon name='home' color={color} size={size}/>
+          tabBarIcon: ({ color, size }) => <Icon name='home' color={color} size={size}/>,
+          headerTitle: (props) => <DropdownCategory {...props} caretType={caretType} setCaretType={setCaretType}/>,
+          headerStyle: {
+            backgroundColor: '#a8c8ff',
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff'
+          }
         }}/>
         <Tab.Screen name='Calendar' component={CalendarScreen} options={{
           title: '달력',
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react'
-import { SafeAreaView, View, Text, StyleSheet, StatusBar, Keyboard } from 'react-native'
+import React, { useEffect, useState, useRef } from 'react'
+import { 
+    SafeAreaView, 
+    View, Text, 
+    StyleSheet, 
+    StatusBar, 
+    Keyboard,
+    FlatList,
+    TouchableHighlight
+} from 'react-native'
 
 import Default from '../components/Default'
 import DateHeader from '../components/DateHeader'
 import TodoInsert from '../components/TodoInsert'
 import TodoList from '../components/TodoList'
+import DropdownItem from '../components/DropdownItem'
 
-function HomeScreen({navigation}){
+function HomeScreen({ navigation, caretType, setCaretType }){
 
     const date = new Date()
+    const categories = ['자기계발', '업무', '오락', '여행', '연애', 'IT', '취미']
     const [todos, setTodos] = useState([
         {id: 1, title: '공원에 산책가기', category: '여행', createdAt: '2023-08-22', isDone: false},
         {id: 2, title: '보고서 작성하기', category: '업무', createdAt: '2023-08-22', isDone: true},
@@ -16,8 +26,15 @@ function HomeScreen({navigation}){
     ])
     const [todoText, setTodoText] = useState('')
     const [warning, setWarning] = useState(false)
+    const category = useRef('')
 
     const onInsertTodo = (trimedText) => {
+        if(!category.current){
+            setTodoText('카테고리를 먼저 선택해주세요')
+            setWarning(true)
+            return
+        }
+
         if(trimedText && trimedText.length > 2){
             const nextId = todos.length + 1
             const todoContents = trimedText.split(',')
@@ -26,7 +43,7 @@ function HomeScreen({navigation}){
             const newTodo = {
                 id: todos.length + 1,
                 title: todoContents[0],
-                category: todoContents[1] || '자기계발',
+                category: category.current || '자기계발',
                 createdAt: `${createdTime.getFullYear()}-${createdTime.getMonth() + 1}-${createdTime.getDate()}`
             }
 
@@ -34,9 +51,10 @@ function HomeScreen({navigation}){
                 setTodoText('할일이 이미 존재합니다')
                 setWarning(true)
             }else{
-                setTodos([...todos, newTodo])
+                setTodos([newTodo, ...todos])
                 Keyboard.dismiss()
                 setTodoText('')
+                category.current = ''
             }
         }else{
             setTodoText('3자 이상 입력하세요')
@@ -44,9 +62,47 @@ function HomeScreen({navigation}){
         }
     }
 
+    const closeDropdown = () => {
+        caretType && setCaretType(false)
+    }
+
+    const handleOutSideOfMenu = () => {
+        closeDropdown()
+    }
+
+    const selectCategory = (item, e) => {
+        closeDropdown()
+        category.current = item
+    }
+
+    useEffect(() => navigation.addListener('focus', () => console.log('페이지 로딩')), [])
+    useEffect(() => navigation.addListener('blur', () => console.log('페이지 벗어남')), [])
+
     return (
-        <SafeAreaView style={styles.block}>
+        <SafeAreaView 
+            style={styles.block}
+            onTouchStart={handleOutSideOfMenu}
+        >
             <StatusBar backgroundColor="#a8c8ffff"></StatusBar>
+
+            {caretType && (
+                <View 
+                    style={styles.dropdownShadow}
+                    onTouchStart={(e) => {
+                        e.stopPropagation()
+                    }}    
+                >
+                    <FlatList
+                        data={categories}
+                        keyExtractor={item => item}
+                        renderItem={({item}) => (
+                            <DropdownItem category={item} selectCategory={(e) => selectCategory(item, e)}/>
+                        )}
+                        style={styles.dropdownList}
+                    />
+                </View>
+            )}
+
             <DateHeader date={date}/>
             {todos.length === 0? <Default/> : <TodoList todos={todos}/>}
             <TodoInsert 
@@ -63,7 +119,22 @@ function HomeScreen({navigation}){
 const styles = StyleSheet.create({
     block: {
         flex: 1
-    }
+    },
+    dropdownList: {
+        padding: 5,
+    },
+    dropdownShadow: {
+        shadowOffset: { width: 0, height: 20 },
+        shadowColor: '#000',
+        shadowOpacity: 0.25,
+        backgroundColor: '#fff',
+        zIndex: 1,
+        elevation: 1,
+        position: 'absolute',
+        top: -15,
+        borderRadius: 5,
+        margin: 15,
+    },
 })
 
 export default HomeScreen
